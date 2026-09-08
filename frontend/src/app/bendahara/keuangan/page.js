4ee1ddc137766f35b9c02e89830e3c06fcce7594
Rais <2406133@itg.ac.id>
2026-09-08 16:11:13 +0700

    Update halaman keuangan bendahara

--- frontend/src/app/bendahara/keuangan/page.js
+++ frontend/src/app/bendahara/keuangan/page.js
@@ -223,12 +223,14 @@ export default function BendaharaKeuanganPage() {
 
             if (res.ok) {
                 fetchBills();
+                alert(status === 'lunas' ? 'Pembayaran berhasil dikonfirmasi!' : 'Pembayaran berhasil dibatalkan!');
             } else {
                 const data = await res.json();
                 alert(data.message || 'Gagal merubah status pembayaran.');
             }
         } catch (err) {
             console.error('Error paying bill:', err);
+            alert('Terjadi kesalahan jaringan.');
         }
     };
 
